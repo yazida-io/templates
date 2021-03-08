89daf1fe1f46382b94a4f9d254c1637b900893c7
Mouctar DIALLO <abdoulmouctard@gmail.com>
2021-03-08 09:28:16 +0100

    component #2

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="bg-gray-50 h-full py-10">
     <div class="bg-white container mx-auto rounded-lg shadow-md p-4">
+      <h1 class="text-4xl text-center font-thin p-2 border-b-2">
+        Freebies UI Components
+      </h1>
       <router-view></router-view>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        ...colors
+      },
       borderRadius: {
         xl: "12px",
         "2xl": "24px",
